3721f35375bc039c6095f7af3860244e14a08061
sa2z <57865727+sa2z@users.noreply.github.com>
2021-07-29 01:42:55 +0900

    async_reducer

--- src/Users.js
+++ src/Users.js
@@ -1,33 +1,43 @@
-import React, { useEffect, useReducer } from "react";
+// import React, { useEffect, useReducer } from "react";
 import axios from "axios";
 import { asyncReducer } from "./asyncReducer";
+import useAsync from "./useAsync";
+
+async function getUsers() {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/users/"
+  );
+  return response.data;
+}
 
 function Users() {
-  const [state, dispatch] = useReducer(asyncReducer, {
-    loading: false,
-    data: null,
-    error: null
-  });
-  const fetchUsers = async () => {
-    dispatch({ type: "LOADING" });
-    try {
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/users/"
-      );
-      dispatch({ type: "SUCCESS", data: response.data });
-    } catch (e) {
-      dispatch({ type: "ERROR", error: e });
-    }
-  };
+  // const [state, dispatch] = useReducer(asyncReducer, {
+  //   loading: false,
+  //   data: null,
+  //   error: null
+  // });
+  // const fetchUsers = async () => {
+  //   dispatch({ type: "LOADING" });
+  //   try {
+  //     const response = await axios.get(
+  //       "https://jsonplaceholder.typicode.com/users/"
+  //     );
+  //     dispatch({ type: "SUCCESS", data: response.data });
+  //   } catch (e) {
+  //     dispatch({ type: "ERROR", error: e });
+  //   }
+  // };
 
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+  // useEffect(() => {
+  //   fetchUsers();
+  // }, []);
+
+  const [state, refetch] = useAsync(getUsers, [], true);
 
   const { loading, data: users, error } = state;
   if (loading) return <div>로딩중 ... </div>;
   if (error) return <div>에러발생 ...</div>;
-  if (!users) return null;
+  if (!users) return <button onClick={refetch}>불러오기</button>;
 
   return (
     <>
@@ -38,7 +48,7 @@ function Users() {
           </li>
         ))}
       </ul>
-      <button onClick={fetchUsers}>다시 불러오기</button>
+      <button onClick={refetch}>다시 불러오기</button>
     </>
   );
 }
