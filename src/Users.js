7a22206aaccd4aa1c9c23455fb0c5ddd5c68a724
sa2z <57865727+sa2z@users.noreply.github.com>
2021-07-29 01:18:38 +0900

    reducer_refactoring

--- src/Users.js
+++ src/Users.js
@@ -1,33 +1,9 @@
 import React, { useEffect, useReducer } from "react";
 import axios from "axios";
+import { asyncReducer } from "./asyncReducer";
 
-// Loading, Success, Error
-function reducer(state, action) {
-  switch (action.type) {
-    case "LOADING":
-      return {
-        loading: true,
-        data: null,
-        error: null
-      };
-    case "SUCCESS":
-      return {
-        loading: false,
-        data: action.data,
-        error: null
-      };
-    case "ERROR":
-      return {
-        loading: false,
-        data: null,
-        error: action.error
-      };
-    default:
-      throw new Error(`Unhandled action type: ${action.type}`);
-  }
-}
 function Users() {
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, dispatch] = useReducer(asyncReducer, {
     loading: false,
     data: null,
     error: null
